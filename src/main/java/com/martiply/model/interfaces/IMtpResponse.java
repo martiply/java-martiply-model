f49ef6a04b475f23ebff2c7852bfeea20b82fd7c
Martin <martin@hanatoya.jp>
2018-12-21 21:04:29 +0900

    Removed Type

--- src/main/java/com/martiply/model/interfaces/IMtpResponse.java
+++ src/main/java/com/martiply/model/interfaces/IMtpResponse.java
@@ -1,9 +1,7 @@
 package com.martiply.model.interfaces;
 
-import java.util.List;
-
-public interface IMtpResponse<T>  {
+public interface IMtpResponse {
 
     public boolean isSuccess();
-    
+
 }
